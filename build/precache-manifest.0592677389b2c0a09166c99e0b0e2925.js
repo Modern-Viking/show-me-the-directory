d8990ccb44ca6c64020c63a76e1e258624a73469
Modern Viking <thomascustoms715@gmail.com>
2020-04-04 11:36:25 -0600

    pushing build

---
+++ build/precache-manifest.0592677389b2c0a09166c99e0b0e2925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a7981084f0c19e1e84e9adfa7c9228",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8faec9ced007d07e2ae7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "174144b4af1e7308693c",
+    "url": "/static/js/2.64b41f9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.64b41f9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8faec9ced007d07e2ae7",
+    "url": "/static/js/main.e36294d9.chunk.js"
+  },
+  {
+    "revision": "469f0d3e59f329f25ece",
+    "url": "/static/js/runtime-main.1b78e3b8.js"
+  }
+]);
