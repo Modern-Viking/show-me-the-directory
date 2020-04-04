bb18c75600cf3b1f3420d70de704fbe1bef54e6a
Modern Viking <thomascustoms715@gmail.com>
2020-04-04 12:49:47 -0600

    i think im bugged too

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 3000
 const publicPath = path.join(__dirname, './public');
 app.use(express.static(publicPath));
 
+require("./routes/htmlRoutes")(app);
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
  });
